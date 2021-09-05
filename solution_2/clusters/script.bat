64124d8b62381d03702f58493d13fce88ee4ee35
Nikola Milicevic <nikola260896@gmail.com>
2021-09-05 02:11:17 +0200

    added solutions

---
+++ solution_2/clusters/script.bat
@@ -0,0 +1,25 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+set cluster=
+
+for /f %%G in ('dir /b /a-d *.txt') do (
+	set /p cluster= < %%G
+	@echo !cluster!
+	if !cluster! leq 15 (
+		START /WAIT C:\Users\mn170387d\Documents\"Visual Studio 2019"\Projects\CudaDiplomski\x64\Release\CudaDiplomski.exe %%G
+	)
+	if !cluster!==16 (
+		START /WAIT C:\Users\mn170387d\Documents\"Visual Studio 2019"\Projects\CudaDiplomski14\x64\Release\CudaDiplomski14.exe %%G
+	)
+	if !cluster!==17 (
+		START /WAIT C:\Users\mn170387d\Documents\"Visual Studio 2019"\Projects\CudaDiplomski15\x64\Release\CudaDiplomski15.exe %%G
+	)
+	if !cluster!==18 (
+		START /WAIT C:\Users\mn170387d\Documents\"Visual Studio 2019"\Projects\CudaDiplomski16\x64\Release\CudaDiplomski16.exe %%G
+	)
+	
+
+)
+
+
